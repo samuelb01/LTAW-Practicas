09ea638495b26808f8ba3dff97ba21f197364421
samuelb01 <samubellon@gmail.com>
2024-02-19 18:36:38 +0100

    Empezando a crear el servidor

--- P1/tienda.js
+++ P1/tienda.js
@@ -14,3 +14,22 @@ BACK-END
 - Tiene que poder servir archivos html, css, javascript e imágenes
 - Si se pide un recurso no disponible tiene que generar un mensaje de error
 */
+
+const http = require('http'); //-- Acceso a los elementos del módulo http 
+const fs = require('fs'); //-- Módulo fs para acceder con Node.js a los ficehro del ordenador
+
+
+//-- Definir puerto a utilizar (9090)
+const PORT = 9090;
+
+//-- Creación del servidor
+const server = http.createServer();
+
+//-- Función de atender
+function atender(req, res) {
+    console.log('CONECTADO');
+}
+
+server.on('request', atender);
+server.listen(PORT);
+
