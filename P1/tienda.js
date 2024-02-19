43800110c8f6cfa126d7dbe0cd3b9c5e6b698266
samuelb01 <samubellon@gmail.com>
2024-02-19 18:19:58 +0100

    Creación archivos html, css y javascript

---
+++ P1/tienda.js
@@ -0,0 +1,16 @@
+/*
+tienda on-line
+crear back-end y front-end
+Documentación técnica y manual de usuario en markdown en la wiki de la P1
+Si se implementan mejoras indicarlo
+*/
+
+/*
+BACK-END
+- Implementar con node.js
+- usar módulos http y fs
+- Nombre del servidor en la carpeta P1 con nombre tienda.js
+- Servidor debe escuchar en el puerto 9090
+- Tiene que poder servir archivos html, css, javascript e imágenes
+- Si se pide un recurso no disponible tiene que generar un mensaje de error
+*/
