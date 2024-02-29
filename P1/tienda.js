bd8317c23066dff745fe5e33b872f42827bd6000
samuelb01 <samubellon@gmail.com>
2024-02-29 23:53:45 +0100

    intentando lectura ficheros

--- P1/tienda.js
+++ P1/tienda.js
@@ -25,38 +25,40 @@ const fs = require('fs');   //-- Módulo fs para acceder con Node.js a los ficeh
 const PORT = 9090;
 
 // Variables de las páginas a mostrar
+// ¿ Crear objeto o diccionario que las guarde todas?
 let pagina_main;
 let pagina_error;
+let producto1;
+let producto2;
+let producto3;
 
-//-- Fichero HTML de la página principal
-fs.readFile('index.html', 'utf-8', (err, data) => {
-    if (err) {  //-- Ocurre un error
-        console.log("ERROR!!")
-        console.log(err.message);
-    }
-    else {  //-- No ha ocurrido ningún error
-        console.log("Lectura completada...");
+// Lee archivos:
+// fichero -> Fichero a leer
+// pagina -> variabl donde se guarda la página
+function leerFicheros(fichero, pagina) {
+    //-- Fichero HTML de la página principal
+    fs.readFile(fichero, 'utf-8', (err, data) => {
+        if (err) {  //-- Ocurre un error
+            console.log("ERROR!!")
+            console.log(err.message);
+        }
+        else {  //-- No ha ocurrido ningún error
+            console.log("Lectura completada...");
 
-        // Al ser una función asíncrona hay que declarar la variable
-        // dentro de la función de callback
-        pagina_main = data;
-    }
-});
+            // Al ser una función asíncrona hay que declarar la variable
+            // dentro de la función de callback
+            pagina = data;
+        }
+    });
+};
 
-//-- Fichero HTML de la página principal
-fs.readFile('error.html', 'utf-8', (err, data) => {
-    if (err) {  //-- Ocurre un error
-        console.log("ERROR!!")
-        console.log(err.message);
-    }
-    else {  //-- No ha ocurrido ningún error
-        console.log("Lectura completada...");
+leerFicheros('index.html', pagina_main);
+leerFicheros('error.html', pagina_error);
+leerFicheros('producto1.html', producto1);
+leerFicheros('producto2.html', producto2);
+leerFicheros('producto3.html', producto3);
 
-        // Al ser una función asíncrona hay que declarar la variable
-        // dentro de la fuinción de callback
-        pagina_error = data;
-    }
-});
+console.log('hola ' + pagina_main);
 
 //-- Se crea el servidor con la función de retrollamada
 const server = http.createServer((req, res) => {
@@ -72,14 +74,25 @@ const server = http.createServer((req, res) => {
     // Se analiza el recurso
     // Se construye el objeto URL con la url de la solicitud
     const url = new URL(req.url, 'http://' + req.headers['host']);
-    
+
+
     // --- Recurso NO página principal ---
-    if (url.pathname != '/') {
+    if (url.pathname == '/producto1') {
+        page = producto1;
+
+    } if (url.pathname == '/producto2') {
+        page = producto2;
+
+    } if (url.pathname == '/producto3') {
+        page = producto3;
+
+    } else if (url.pathname != '/') {
         code = 404;             // Código de respuesta
         code_msg = "Not Found"; // Mensaje asociado al código
         page = pagina_error;    // Página asociada
     }
     // -----------------------------------
+
 
     // Generar la respuesta en función de las variables
     // code, code_msg y page
