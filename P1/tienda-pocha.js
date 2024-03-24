edb54aef7e667f71f4d33b9286a5798d23b40cd7
samuelb01 <samubellon@gmail.com>
2024-03-24 22:29:41 +0100

    Más cambios

--- P1/tienda-pocha.js
+++ P1/tienda-pocha.js
@@ -6,26 +6,69 @@ const { url } = require('inspector');
 // Puerto que se va a utilizar (9090)
 const PORT = 9090;
 
+const pagina_error = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi tienda</title>
+</head>
+<body style="background-color: red">
+    <h1 style="color: white">ERROR!!!!</h1>
+</body>
+</html>`
+
+
+// Función para leer archivos
+function leerFichero(fichero, callback) {
+    fs.readFile(fichero, (err, data) => {
+        if (err) {
+            console.error("ERROR al leer el archivo:", fichero, err);
+            callback(err, null);
+        } else {
+            console.log(`Lectura completada de ${fichero}`);
+            callback(null, data);
+        }
+    });
+}
+
+
 const server = http.createServer((req, res) => {
     console.log("Petición recibida:", req.url);
 
     // Analizar recurso
-    if (req.url == '/') {
+    if (req.url.ends) {
         recurso = './Pages/hola.html';
     } else {
         recurso = '.' + req.url;
     }
-    console.log(recurso);
+    console.log(recurso);  // QUITAR
 
     
     // Declarar el Content-Type
-    if (req.url.endsWith('.png')) {
+    if (req.url.endsWith('.png')) {  //--PNG
         content_type = 'image/png';
+        recurso = '.' + req.url;
+    } else if (req.url.endsWith('.html')) {
+        content_type = 'text/html'
+        recurso = './Pages/hola.html'
+    } else if (req.url.endsWith('.css')) {  //--CSS
+        content_type = 'text/css';
+        recurso = '.' + req.url;
+    } else if (req.url.endsWith('.js')) {  //--JS
+        content_type = 'application/javascript';
+        recurso = '.' + req.url;
+    } else if (req.url.endsWith('.jpeg')) {  //--JPEG
+        content_type = 'image/jpeg';
+        recurso = '.' + req.url;
     } else {  // Valor por defecto -> HTML
         content_type = 'text/html'
+        recurso = './Pages/hola.html'
     }
 
-    fs.readFile(recurso, (err, data) => {
+    leerFichero(recurso, (err, data) => {
 
         if (err) {
             res.statusCode = 404;             // Código de respuesta
@@ -39,8 +82,6 @@ const server = http.createServer((req, res) => {
             res.setHeader('Content-Type', content_type);
             res.write(data);
             res.end();
-
-            console.log('Has enviaddo algo');
         }
 
     });
