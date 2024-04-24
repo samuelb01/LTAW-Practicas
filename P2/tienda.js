3f3593eb57b2ccb9ccb2ce50e68e15ca13955d6d
samuelb01 <samubellon@gmail.com>
2024-04-24 10:43:36 +0200

    cambios en la tienda.js

--- P2/tienda.js
+++ P2/tienda.js
@@ -1,30 +1,31 @@
-// Importar módulos
+//-- Importar módulos
 const http = require('http');   //-- Acceso a los elementos del módulo http 
 const fs = require('fs');   //-- Módulo fs para acceder con Node.js a los fichero del ordenador
 const { url } = require('inspector');
 
 
-// Puerto que se va a utilizar (9090)
+//-- Puerto que se va a utilizar (9090)
 const PORT = 9090;
 
+//-- Nombre de las paginas a leer y enviar
 const pagina_error = fs.readFileSync('./Pages/pagina-error.html', 'utf8');
 const FORMULARIO_LOGIN = './Pages/login.html';
 const LOGIN_CORRECTO = './Pages/login-correcto.html'
 const LOGIN_NO_CORRECTO = './Pages/login-no-correcto.html'
 const PAGINA_PRODUCTOS = './Pages/pagina-producto.html'
 
-const tienda_json = fs.readFileSync('./tienda.json', 'utf-8');
 
-// Creo la estructura de la tienda
+//-- Cargo el archivo .json y creo la estructura de la tienda
+const tienda_json = fs.readFileSync('./tienda.json', 'utf-8');
 const tienda = JSON.parse(tienda_json);
 
-// Info de la tienda
+//-- Info de la tienda
 let productos = tienda.productos;
 let usuarios = tienda.usuarios;
 let pedidos = tienda.pedidos;
 
 
-// Función para leer archivos
+//-- Función para leer archivos
 function leerFichero(fichero, callback) {
     fs.readFile(fichero, (err, data) => {
         if (err) {
@@ -37,6 +38,7 @@ function leerFichero(fichero, callback) {
     });
 }
 
+//-- función para mandar 404 Not Found
 function code_404 (res) {
     res.statusCode = 404;             // Código de respuesta
     res.statusMessage = "Not Found"; // Mensaje asociado al código
@@ -45,8 +47,8 @@ function code_404 (res) {
     res.end();
 }
 
-function code_200 (res, data) {
-    //-- Envía del rescurso procesado
+//-- Función para mandar 200 OK
+function code_200 (res, data, content_type) {
     res.statusCode = 200;         // Código de respuesta
     res.statusMessage = "OK";    // Mensaje asociado al código
     res.setHeader('Content-Type', content_type);
@@ -54,29 +56,26 @@ function code_200 (res, data) {
     res.end();
 }
 
-
+//-- Se crea el servidor
 const server = http.createServer((req, res) => {
     const myURL = new URL(req.url, 'http://' + req.headers['host']);
 
     console.log("Petición recibida:", myURL.pathname);
     
-    // Declarar el Content-Type y recurso
+    //-- Declarar el Content-Type y recurso
     if (myURL.pathname == '/procesar') {  //-- Login
-        content_type = 'text/html';
-        
-        // Quiero comprobar si el usuario está en el json
-        // Primero obtengo de la solicitud el valor del nombrey la contraseña
+
+        //-- Quiero comprobar si el usuario está en el json
+        //-- Primero obtengo de la solicitud el valor del nombrey la contraseña
         username = myURL.searchParams.get('username');
         password = myURL.searchParams.get('password');
-        console.log('USUARIO: ' + username);
-        console.log('PASSWORD: ' + password);
 
-        usuario_existe = false;
+        usuario_existe = false; //-- Condición para solicitar o no al usuario que se registre en la tienda
 
-        usuarios.forEach(element => { // Itera sobre todos los usuarios para ver si está registrado
-            // Si el usuario introducido y la contraseña coinciden, se da como login correcto
-            // Si el usuario existe pero la contraseño no es correcto se pide que se vuelva a introducir
-            // Si el usuario no xiste se pide que se registre o si prefiere seguir navegando
+        usuarios.forEach(element => { //-- Itera sobre todos los usuarios para ver si está registrado
+            //-- Si el usuario introducido y la contraseña coinciden, se da como login correcto
+            //-- Si el usuario existe pero la contraseño no es correcto se pide que se vuelva a introducir
+            //-- Si el usuario no xiste se pide que se registre o si prefiere seguir navegando
             if (element.nombre_usuario == username) {
                 if (element.password == password) {  //-- Contraseña correcta
                     leerFichero(LOGIN_CORRECTO, (err, data) => {
@@ -86,8 +85,9 @@ const server = http.createServer((req, res) => {
                             data = data.toString();
                             data = data.replace("NOMBRE", username);
 
-                            //-- Envía del rescurso procesado
-                            code_200(res, data);
+                            //-- Envío del rescurso procesado
+                            content_type = 'text/html';
+                            code_200(res, data, content_type);
                         }
                     });
 
@@ -99,17 +99,18 @@ const server = http.createServer((req, res) => {
                             data = data.toString();
                             data = data.replace("Accede al FunkoVerse", "Contraseña incorrecta");
 
-                            //-- Envía del rescurso procesado
-                            code_200(res, data);
+                            //-- Envío del rescurso procesado
+                            content_type = 'text/html';
+                            code_200(res, data, content_type);
                         }
                     });
                 }
 
-                usuario_existe = true;
+                usuario_existe = true;  //-- El usuario exiiste en la bse de datos -> No se pregunta por registro
             }
         });
 
-        // Si el usuario no exisitia en la base de datos se pregunta por regitro
+        //-- Si el usuario no exisitia en la base de datos se pregunta por regitro
         if (usuario_existe == false) {
             leerFichero(LOGIN_NO_CORRECTO, (err, data) => {
                 if (err) {
@@ -119,8 +120,9 @@ const server = http.createServer((req, res) => {
                     data = data.replace("AVISO", "USUARIO NO REGISTRADO");
                     data = data.replace("CUERPO-AVISO", "Puede decidir si registrarse o seguir navegando como invitado:"); 
     
-                    //-- Envía del rescurso procesado
-                    code_200(res, data);
+                    //-- Envío del rescurso procesado
+                    content_type = 'text/html';
+                    code_200(res, data, content_type);
                 }
             });
         }
@@ -161,7 +163,8 @@ const server = http.createServer((req, res) => {
                 data = data.replace("Bienvenido: NOMBRE", "¡Gracias por confiar en nosotros!");
         
                 //-- Envía del rescurso procesado
-                code_200(res, data);
+                content_type = 'text/html';
+                code_200(res, data, content_type);
             }
         });
 
@@ -199,7 +202,8 @@ const server = http.createServer((req, res) => {
                 data = data.replace("NOMBRE", username)
                 
                 //-- Envía del rescurso procesado
-                code_200(res, data);
+                content_type = 'text/html';
+                code_200(res, data, content_type);
             }
         });
 
@@ -232,53 +236,54 @@ const server = http.createServer((req, res) => {
                 data = data.replace('<!-- IMAGEN -->', '<img src="../Images/PumpkinKing-box.png" alt="PumpkinKing-box">')
 
                 //-- Envía del rescurso procesado
-                code_200(res, data);
+                content_type = 'text/html';
+                code_200(res, data, content_type);
             }
         });
 
     } else if (myURL.pathname.endsWith('.png')) {  //--PNG
         content_type = 'image/png';
         recurso = './Images/' + myURL.pathname.split('/').pop();
-        enviarFicheros(recurso);
+        enviarFicheros(recurso, content_type);
 
     } else if (myURL.pathname.endsWith('.html')) {  //-- HTML
         content_type = 'text/html'
         recurso = './Pages/' + myURL.pathname.split('/').pop();
-        enviarFicheros(recurso);
+        enviarFicheros(recurso, content_type);
 
     } else if (myURL.pathname.endsWith('.css')) {  //--CSS
         content_type = 'text/css';
         recurso = './Style/' + myURL.pathname.split('/').pop();
-        enviarFicheros(recurso);
+        enviarFicheros(recurso, content_type);
 
     } else if (myURL.pathname.endsWith('.js')) {  //--JS
         content_type = 'application/javascript';
         recurso = './JS/' + myURL.pathname.split('/').pop();
-        enviarFicheros(recurso);v
+        enviarFicheros(recurso, content_type);v
 
     } else if (myURL.pathname.endsWith('.jpeg')) {  //--JPEG
         content_type = 'image/jpeg';
         recurso = './Images/' + myURL.pathname.split('/').pop();
-        enviarFicheros(recurso);
+        enviarFicheros(recurso, content_type);
 
     } else if (myURL.pathname == '/') {
         content_type = 'text/html';
         recurso = './Pages/tienda.html';
-        enviarFicheros(recurso);
+        enviarFicheros(recurso, content_type);
 
     } else {  // Valor por defecto -> HTML - error.html
         content_type = 'text/html';
         recurso = pagina_error;
-        enviarFicheros(recurso);
+        enviarFicheros(recurso, content_type);
     }
 
-    function enviarFicheros(recurso) {
+    function enviarFicheros(recurso, content_type) {
         leerFichero(recurso, (err, data) => {
             if (err) {
                 code_404(res);
             } else {
                 //-- Envía del rescurso procesado
-                code_200(res, data);
+                code_200(res, data, content_type);
             }
         });
     };
