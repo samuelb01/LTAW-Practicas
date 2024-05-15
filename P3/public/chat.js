357ed9ba03c0655eabe31188e96426a180dee7c0
samuelb01 <samubellon@gmail.com>
2024-05-15 18:09:00 +0200

    cambios

---
+++ P3/public/chat.js
@@ -0,0 +1,34 @@
+//-- Crear un websocket. Se establece la conexión con el servidor
+// const socket = io();//-- Elementos del interfaz
+const display = document.getElementById("display");
+const msg_entry = document.getElementById("msg_entry");
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
+
+const urlParams = new URLSearchParams(window.location.search);
+const username = urlParams.get('username');
+
+let contador = 1;
+
+socket.on("connect", () => {
+    //-- Enviar mensaje inicial
+    socket.send("Mensaje inicial del Cliente!!!");
+});         
+
+socket.on("disconnect", ()=> {
+    display.innerHTML="¡¡DESCONECTADO!!"
+})
+
+socket.on("message", (msg)=>{
+    display.innerHTML += '<p style="color:blue">' + msg + '</p>';
+});
+
+//-- Al apretar el botón se envía un mensaje al servidor
+msg_entry.onchange = () => {
+    if (msg_entry.value)
+    socket.send(msg_entry.value);
+
+    //-- Borrar el mensaje actual
+    msg_entry.value = "";
+}
